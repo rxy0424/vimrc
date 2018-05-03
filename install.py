83a654f45322059e480010cec700525803f472d8
rxy0424 <rxy0424@gmail.com>
2018-05-03 10:55:41 +0800

    modifiy for python3

--- install.py
+++ install.py
@@ -5,8 +5,7 @@ vimrc_location =  sys.path[0]
 user_home = os.environ.get('HOME')
 
 for file_name in os.listdir(vimrc_location):
-  import pdb; pdb.set_trace()  # XXX BREAKPOINT
   if file_name[0] == '.' and os.path.isfile(vimrc_location + '/' + file_name):
     cmd_to_run = 'ln -sf ' + vimrc_location + '/' + file_name + " " + user_home + '/' + file_name
-    print cmd_to_run
+    print (cmd_to_run)
     os.system(cmd_to_run)
